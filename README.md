a7e9cfc8571cbe3a978e36daa56eb80a0b7cf422
woody1983 <unix1983@gmail.com>
2012-12-12 10:28:56 +0800

    First

---
+++ README.md
@@ -0,0 +1,3 @@
+## Doorbell
+
+>Doorbell is Simple Authentication Function
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doorbell::Application.config.secret_token = 'dae81b3d5d687acb9eaaa703f290535a79e13610544bda0f309bcb96c7a74488777d12dd8f09c3b280a4a8888926352f8d11c88e092857c615794c4c56b3b2bb'
